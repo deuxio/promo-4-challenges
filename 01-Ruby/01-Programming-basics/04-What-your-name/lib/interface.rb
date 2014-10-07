94329870254f60269d10d7595ddd77abc35bd885
Brice Maurin <brice@deux.io>
2014-10-07 13:55:32 +0200

    badaboum.

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,8 +1,19 @@
 require_relative "compute_name"
 
 # TODO: ask for the first name
-# TODO: ask for the middle name
-# TODO: ask for the last name
-# TODO: Print a greeting to the user with the full name concatenated,
-#       something like "Hello, Boris Paillard"
+puts "What\'s your first name?"
+first_name = gets.chomp
 
+# TODO: ask for the middle name
+puts "What\'s your middle name?"
+middle_name = gets.chomp
+
+# TODO: ask for the last name
+puts "What\'s your last name?"
+last_name = gets.chomp
+
+# TODO: Print a greeting to the user with the full name concatenated,
+# something like "Hello, Boris Paillard"
+
+full_name = compute_name(first_name, middle_name, last_name)
+puts "Greetings #{full_name}"
