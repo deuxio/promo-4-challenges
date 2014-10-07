3a0b421438b9210e81c64dba81820fa641a47798
Brice Maurin <brice@deux.io>
2014-10-07 15:21:34 +0200

    ouch.

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,25 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message.upcase == "I AM GOING TO WORK RIGHT NOW!"
+    ""
+  elsif your_message[-1,1] == "?"
+    "Silly question, get dressed and go to work!"
+  else
+    "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
+  if your_message == "I AM GOING TO WORK RIGHT NOW!"
+    coach_enhanced_message = ""
+  elsif your_message.upcase == your_message
+    coach_enhanced_message = "I can feel your motivation! "
+  else
+    coach_enhanced_message = ""
+  end
+
+  coach_enhanced_message + coach_answer(your_message)
+
 end
 
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,10 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+  puts "Qu'est ce que tu veux?"
+  your_message = gets.chomp
+
+  unless your_message == "I am going to work right now!"
+    puts coach_answer_enhanced(your_message)
+    else
+    ""
+  end
