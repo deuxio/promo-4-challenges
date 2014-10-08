2a5ea5c99f76e6a26bf4a66400fc804baec689cd
Brice Maurin <brice@deux.io>
2014-10-08 15:20:17 +0200

    en version récursive

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -4,8 +4,32 @@ require_relative "wagon_sort"
 #       Remember, to read an input from the command line, use:
 #       - `gets`:  http://www.ruby-doc.org/core-2.1.2/Kernel.html#method-i-gets
 #       - `chomp`: http://www.ruby-doc.org/core-2.1.2/String.html#method-i-chomp
+student_list = []
 
+def question(answer)
+  student_temp =[]
+  if answer != ""
+    student_temp << answer
+    puts "Type another student name (or press enter to finish):"
+    answer = gets.chomp
+    student_temp + question(answer)
+  else
+    student_temp
+  end
+end
 
+puts "Type a student name:"
+answer = gets.chomp
+student_list = question(answer)
+
+puts "Congratulations! Your Wagon has #{student_list.size} students:"
+
+first = wagon_sort(student_list).take(student_list.size - 1).join(", ")
+puts first
+last = wagon_sort(student_list)[-1]
+puts last
+
+puts "- #{first} and #{last}"
 
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
