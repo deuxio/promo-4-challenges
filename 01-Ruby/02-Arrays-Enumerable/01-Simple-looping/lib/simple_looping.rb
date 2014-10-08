5376ef4dbcf5a333c8afbd46f3470faa74a69f74
Brice Maurin <brice@deux.io>
2014-10-08 12:22:02 +0200

    manque le recursif...je passe a la suite et y reviendrais

--- 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
@@ -1,12 +1,33 @@
 def sum_with_while(min, max)
+  # TODO: return the sum of the integers between min and max
   # CONSTRAINT: you should use a while..end structure
+  count = min
+  sum = 0
+
+  while count <= max
+    sum += count
+    count += 1
+  end
+  sum
 end
 
 def sum_with_for(min, max)
+  # TODO: return the sum of the integers between min and max
   # CONSTRAINT: you should use a for..end structure
+  sum = 0
+
+  for num in min..max
+    sum += num
+  end
+  sum
 end
 
 def sum_recursive(min, max)
+  # TODO (optional): return the sum of the integers between min and max
   # CONSTRAINT: you should use a recursive algorithm, i.e. the method should call itself
-end
 
+  return sum if (min > max)
+  sum += min
+  min += 1
+  sum_recursive(min, max)
+end
