727368b7275ce139ee7d1c9a2ad1c750687d4fbc
Brice Maurin <brice@deux.io>
2014-10-08 15:47:47 +0200

    avec une version plus simple (mais je garde la récursive qd meme en commentaire

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -1,33 +1,46 @@
 require_relative "wagon_sort"
 
-# TODO: Ask the user about students to add to the Wagon.
-#       Remember, to read an input from the command line, use:
-#       - `gets`:  http://www.ruby-doc.org/core-2.1.2/Kernel.html#method-i-gets
-#       - `chomp`: http://www.ruby-doc.org/core-2.1.2/String.html#method-i-chomp
-student_list = []
+# EN VERSION RECURSIF
+# student_list = []
 
-def question(answer)
-  student_temp =[]
-  if answer != ""
-    student_temp << answer
-    puts "Type another student name (or press enter to finish):"
-    answer = gets.chomp
-    student_temp + question(answer)
+# def question(answer)
+#   student_temp =[]
+#   if answer != ""
+#     student_temp << answer
+#     puts "Type another student name (or press enter to finish):"
+#     answer = gets.chomp
+#     student_temp + question(answer)
+#   else
+#     student_temp
+#   end
+# end
+
+# puts "Type a student name:"
+# answer = gets.chomp
+# student_list = question(answer)
+
+# EN VERSION PLUS SIMPLE
+student_list = []
+name = nil
+
+while name != ""
+  if student_list.empty?
+    puts "Type a student name:"
   else
-    student_temp
+    puts "Type another student name (or press enter to finish):"
+  end
+
+  name = gets.chomp
+
+  if name != ""
+    student_list << name
   end
 end
-
-puts "Type a student name:"
-answer = gets.chomp
-student_list = question(answer)
 
 puts "Congratulations! Your Wagon has #{student_list.size} students:"
 
 first = wagon_sort(student_list).take(student_list.size - 1).join(", ")
-puts first
 last = wagon_sort(student_list)[-1]
-puts last
 
 puts "- #{first} and #{last}"
 
