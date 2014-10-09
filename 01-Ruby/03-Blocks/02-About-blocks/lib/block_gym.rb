48691416dfafec202606c72ec5421cec73ee7d8e
Brice Maurin <brice@deux.io>
2014-10-09 14:41:27 +0200

    poum

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,8 +1,35 @@
 def tag(tag_name, attr = nil)
-  # TODO:  Build HTML tags around  content given in the block
+  # TODO:  Build HTML tags around content given in the block
   #        The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  line = "<" + tag_name
+  attr.nil? ? "" : line += " " + attr[0] + "='" + attr[1] + "'"
+  line += ">"
+  line += yield
+  line += "</" + tag_name + ">"
+  # puts line
 end
 
 def timer_for
   # TODO:  Return time taken to execute the given block
+  start_time = Time.now
+  yield
+  Time.now - start_time
 end
+
+# tag("h1", ["id", "title"]) do
+#   "Some Title"
+# end
+
+# tag("h2") do
+#   "Some Title"
+# end
+
+# r = tag("div", ["class", "kitt-container"]) do
+#   tag("a", ["href", "http://kitt.lewagon.org"]) do
+#     tag("h2") do
+#       "KITT"
+#     end
+#   end
+# end
+
+# p r
