f1cc8c5251a50725de7422040fedbf03882c40c1
Brice Maurin <brice@deux.io>
2014-10-10 14:23:08 +0200

    pim

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,9 @@
 def array_to_hash(array)
   #TODO: implement the method :)
+  hash = {}
+
+  array.each_with_index.map do |key, index|
+    block_given? ? hash[yield(index)] = key : hash[index] = key
+  end
+  p hash
 end
