4408d0e10446dd5b4caa2977d0d915781dd1ce55
Brice Maurin <brice@deux.io>
2014-10-10 10:41:37 +0200

    blabla

--- 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
@@ -1,7 +1,41 @@
-def old_roman_numeral(an_integer)
-  # TODO: translate integer in roman number - old-style way
+def old_roman_numeral(an_integer, roman = nil)
+  # TODO: translate integer in roman number
+  if an_integer >= 1000
+    an_integer -= 1000
+    roman.nil? ? roman = "M" : roman += "M"
+    return old_roman_numeral(an_integer, roman)
+  elsif an_integer >= 500
+    an_integer -= 500
+    roman.nil? ? roman = "D" : roman += "D"
+    return old_roman_numeral(an_integer, roman)
+  elsif an_integer >= 100
+    an_integer -= 100
+    roman.nil? ? roman = "C" : roman += "C"
+    return old_roman_numeral(an_integer, roman)
+  elsif an_integer >= 50
+    an_integer -= 50
+    roman.nil? ? roman = "L" : roman += "L"
+    return old_roman_numeral(an_integer, roman)
+  elsif an_integer >= 10
+    an_integer -= 10
+    roman.nil? ? roman = "X" : roman += "X"
+    return old_roman_numeral(an_integer, roman)
+  elsif an_integer >= 5
+    an_integer -= 5
+    roman.nil? ? roman = "V" : roman += "V"
+    return old_roman_numeral(an_integer, roman)
+  else
+    an_integer -= 1
+    roman.nil? ? roman = "I" : roman += "I"
+    return old_roman_numeral(an_integer, roman)
+  end
+
+  puts roman
+
 end
 
-def new_roman_numeral(an_integer)
-  # TODO: translate integer in roman number - modern-style way
-end
+old_roman_numeral(2341)
+
+#def new_roman_numeral(an_integer)
+  # TODO: translate integer in roman an_integer
+#end
