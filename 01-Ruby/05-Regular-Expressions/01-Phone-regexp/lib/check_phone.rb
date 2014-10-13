6567021186b6f8e9a24ae44efec4b83e4016bd18
Brice Maurin <brice@deux.io>
2014-10-13 12:34:46 +0200

    pouf

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,6 +1,5 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
   phone_number = phone_number.gsub(/\s|-/, "")
-  phone_number =~ /(\+336[\d]{8}|06\d{8})/ ? true : false
+  phone_number =~ /(\+33|0)[1-9]\d{8}/ ? true : false
 end
-
--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,7 +1,25 @@
 def anagrams?(a_string, another_string)
   # TODO: implement the obvious method to test if two words are anagrams
+  a_string = a_string.gsub(/\W/,"").downcase
+  another_string = another_string.gsub(/\W/,"").downcase
+  a_string.split(//).sort == another_string.split(//).sort
 end
 
 def anagrams_on_steroids?(a_string, another_string)
   # TODO: implement the improved method
+  a_string = a_string.gsub(/\W/,"").downcase
+  another_string = another_string.gsub(/\W/,"").downcase
+
+  a_string_hash = Hash.new(0)
+  another_string_hash = Hash.new(0)
+
+  a_string.scan(/\w/).each {|a| a_string_hash[a] += 1}
+  another_string.scan(/\w/).each {|key| another_string_hash[key] += 1}
+
+  a_string_hash == another_string_hash
+
 end
+
+  # "Monica Lewinsky!".gsub(/\W/,"").downcase
+  # "Nice silky woman".gsub(/\W/,"").downcase
+
