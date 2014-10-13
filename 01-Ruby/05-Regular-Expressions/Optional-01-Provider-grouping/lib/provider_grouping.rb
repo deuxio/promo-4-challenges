d6793556fc64bbaf7974cf1ea1e87c917197e0d4
Brice Maurin <brice@deux.io>
2014-10-13 16:02:05 +0200

    blablabla

--- 01-Ruby/05-Regular-Expressions/Optional-01-Provider-grouping/lib/provider_grouping.rb
+++ 01-Ruby/05-Regular-Expressions/Optional-01-Provider-grouping/lib/provider_grouping.rb
@@ -1,7 +1,16 @@
 def group_mails(emails)
   # TODO: group email by provider
+  e_hash = {}
+  emails.each do |email|
+    next if email.match(/\n/)
+    email.scan(/((\w+.+)@(\w+).\w+)/)
+    e_hash[$3] = [] unless e_hash[$3]
+    e_hash[$3] << $1
+  end
+  e_hash
 end
 
 def provider?(email, provider)
   # TODO: return true if email is of given provider
+  email =~ /#{provider}/ ? true : false
 end
