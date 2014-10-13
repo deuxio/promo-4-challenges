7db6568f3f37b088b82621ba9751b570553b3352
Brice Maurin <brice@deux.io>
2014-10-13 17:26:22 +0200

    blablabla

--- 01-Ruby/05-Regular-Expressions/Optional-02-A-translation-story/lib/translation.rb
+++ 01-Ruby/05-Regular-Expressions/Optional-02-A-translation-story/lib/translation.rb
@@ -19,6 +19,22 @@ STRINGS = {
   }
 }
 
-def translation(a_string, a_language)
+def translation(a_string, a_language = "en")
   # TODO: your code goes here
+  path = a_string.split(".")
+  translated_text = Hash.new("")
+
+  for i in 0..path.length - 1
+    if i == 0
+      STRINGS[path[i].to_sym].nil? ? translated_text : translated_text = STRINGS[path[i].to_sym]
+    else
+      translated_text[path[i].to_sym].nil? ? translated_text : translated_text = translated_text[path[i].to_sym]
+    end
+  end
+  translated_text[a_language.to_sym].nil? ? "" : translated_text[a_language.to_sym]
 end
+
+# p translation('home.intro', 'fr') # => 'Bienvenue sur Le Wagon'
+# p translation('home.intro', 'es') # => 'Welcome on Le Wagon'
+# p translation('home.content.goodbye') # => 'Goodbye'
+p translation('unvalid.path','en') # => ''
