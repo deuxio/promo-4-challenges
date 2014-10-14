1a00e930c711c92bba1b1ee4e41d80bb164bcbb7
Brice Maurin <brice@deux.io>
2014-10-14 15:14:04 +0200

    tada

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -2,9 +2,34 @@ require 'open-uri'
 require 'json'
 
 def generate_grid(grid_size)
-  # TODO: generate random grid of letters
+  alphabet = ('a'..'z').to_a
+  grid = []
+  grid_size.times { grid << alphabet.sample.upcase }
+  grid
 end
 
 def run_game(attempt, grid, start_time, end_time)
   # TODO: runs the game and return detailed hash of result
+  result = {}
+  result[:time] = end_time - start_time
+
+
+  if attempt.downcase.split(//).all? { |letter| grid.map(&:downcase).include? letter }
+    url = "http://api.wordreference.com/0.8/80143/json/enfr/#{attempt.downcase}"
+    open(url) do |stream|
+      translations = JSON.parse(stream.read)
+      if translations['Error']
+        result[:message] = "not an english word"
+        result[:score] = 0
+      else
+        result[:translation] = translations['term0']['PrincipalTranslations']['0']['FirstTranslation']['term']
+        result[:score] = attempt.length**2 - result[:time]**2.to_i
+        result[:message] = "well done"
+      end
+    end
+  else
+    result[:score] = 0
+    result[:message] = "not in the grid"
+  end
+  result
 end
