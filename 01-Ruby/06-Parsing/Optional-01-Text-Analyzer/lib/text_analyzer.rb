02d3e7fdee270a70349d48f10c51febfc133d20f
Brice Maurin <brice@deux.io>
2014-10-14 15:50:45 +0200

    poum

--- 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
+++ 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
@@ -1,13 +1,13 @@
-def analyze(text)
-  # TODO: should analyze the text, and return the result hash with all features
+def analyze(t)
+  # TODO: should analyze the t, and return the result hash with all features
   {
-    character_count: text.split(//).length,
-    line_count: text.split(/\n/).length,
-    word_count: text.scan(/\w+/).length,
-    sentence_count: text.split(/\.|\?|!/).length,
-    paragraph_count: text.split(/\n\n/).length,
-    average_words_per_sentence: text.split(/\.|\?|!/).length == 0 ? 1 : text.scan(/\w+/).length / text.split(/\.|\?|!/).length,
-    average_sentences_per_paragraph: text.split(/\n\n/).length == 0 ? 1 : text.split(/\.|\?|!/).length / text.split(/\n\n/).length
+    character_count: t.split(//).count,
+    line_count: t.split(/\n/).count,
+    word_count: t.scan(/\w+/).count,
+    sentence_count: t.split(/\.|\?|!/).count,
+    paragraph_count: t.split(/\n\n/).count,
+    average_words_per_sentence: t.split(/\.|\?|!/).count == 0 ? 1 : t.scan(/\w+/).count / t.split(/\.|\?|!/).count,
+    average_sentences_per_paragraph: t.split(/\n\n/).count == 0 ? 1 : t.split(/\.|\?|!/).count / t.split(/\n\n/).count
   }
 end
 
