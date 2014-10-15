02c9c463c72a174f00c9edcb75ca32eb26139d06
Brice Maurin <brice@deux.io>
2014-10-15 11:15:16 +0200

    blabla

---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/job.rb
@@ -0,0 +1,18 @@
+class Job
+  # attr_accessor = :title, :company, :desc, :income_mth
+
+  def initialize(title, company, desc, income_mth)
+    @title = title
+    @desc = desc
+    @company = company
+    @income_mth = income_mth
+  end
+
+  def incomesup?(price)
+    @income_mth > price
+  end
+end
+
+# require_relative "lib/job.rb"
+# cdp_deuxio = Job.new('chef de projet', 'deuxio', 'blabla ablablabla balbalba', 45000)
+# cdp_deuxio.incomesup?(50000)
