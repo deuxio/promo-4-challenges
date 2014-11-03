219d39d54a5a96a0088752e7291a40f649bfe1cb
Brice Maurin <brice@deux.io>
2014-11-03 12:10:26 +0100

    poum

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -1,5 +1,16 @@
-/*
-  *
-  * TODO: add your code here!
-  *
-*/
+// show popup at sartup
+$(function() {
+  $('#message').hide();
+  $('#message').delay(500).slideDown();
+
+
+  // disappear when click
+  $('#message').on('click', function(event) {
+    $('#message').delay(500).slideUp();
+  })
+
+  // re-appear when mouse is over #message-wrapper
+  $('#message-wrapper').on('mouseenter', function(event) {
+    $('#message').delay(500).slideDown();
+  })
+});
