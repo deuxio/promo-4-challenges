240cd001b1369b26208be2423cc5ff2449463eeb
Brice Maurin <brice@deux.io>
2014-11-06 16:13:17 +0100

    poum

--- 04-Front-End/06-Javascript-Advanced/01-Form-Validation/form_validation.js
+++ 04-Front-End/06-Javascript-Advanced/01-Form-Validation/form_validation.js
@@ -1,3 +1,37 @@
-// Your validation code will go in there.
-// Write your validation functions, and bind events
-// in a jQuery DOM ready callback
+var zipCodeRegExp = /^\d{5}$/g
+var emailRegExp = /^\w+@\w+\.\w{2,3}$/g
+var phoneRegExp = /^(0|\+33)(6|7)(?:\.|-| )?\d{2}(?:\.|-| )?\d{2}(?:\.|-| )?\d{2}(?:\.|-| )?\d{2}(?:\.|-| )?$/g
+
+function formValid() {
+  // Check all text filled
+  if ($('input[type=text]').filter(function() { return this.value == ""; }).length > 0) {
+    return false;
+  }
+  // Check zipcode pattern
+  if(! zipCodeRegExp.test( $('#zip_code').val() ) ) {
+    return false;
+  }
+  // Check email pattern
+  if(! emailRegExp.test( $('#email').val() ) ) {
+    return false;
+  }
+  // TOS checked
+  if (!$('#tos').is(":checked")) {
+    return false;
+  }
+  // Check mobile Phone pattern
+  if(! phoneRegExp.test( $('#mobile_phone').val() ) ) {
+    return false;
+  }
+  return true;
+}
+
+function enableButton() {
+  if (formValid()) {
+    $( "button" ).removeProp('disabled');
+  } else {
+    $( "button" ).attr('disabled', true);
+  }
+}
+
+$('input, textarea').on('focusout change', enableButton);
